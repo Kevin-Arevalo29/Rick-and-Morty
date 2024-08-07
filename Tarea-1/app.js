3102be497eca23ad65afa3b25c5429e7a809c12c
Kevin Arevalo <kevinareavalo2904@gmai.com>
2024-08-06 20:40:00 -0500

    Elimino carpeta 'Tarea-1' que fue subida por error

--- Tarea-1/app.js
+++
@@ -1,8 +0,0 @@
-let div = document.getElementById('latti')
-div.innerHTML = 'Esto viene de js'
-div.style.background = 'black'
-div.style.color = 'white'
-div.style.margin = '10px'
-div.style.textAlign = 'center'
-div.style.fontSize = '50px'
-div.style.fontFamily = 'calibri'
